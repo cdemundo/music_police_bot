a7945e93d5f303e87ebf8838a55c8362abf4dd20
cdemundo <cdemundo@gmail.com>
2018-12-19 23:04:05 -0500

    Initial updates to handle messages

--- app.py
+++ app.py
@@ -35,10 +35,11 @@ def _event_handler(event_type, slack_event):
     # ================ Team Join Events =============== #
     # When the user first joins a team, the type of event will be team_join
     if event_type == "message":
-        user_id = slack_event["event"]["user"]["id"]
+        channel_id = slack_event["event"]["channel"]
+        text = slack_event["event"]["text"]
+        ts = slack_event["event"]["ts"]
         # Send the onboarding message
         pyBot.onboarding_message(team_id, user_id)
-        return make_response("Welcome Message Sent", 200,)
 
     # ============== Share Message Events ============= #
     # If the user has shared the onboarding message, the event type will be
