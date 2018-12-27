4e08fac98d0d421819f36803b66d694a40bce7d6
cdemundo <cdemundo@gmail.com>
2018-12-26 22:32:26 -0500

    Cleaning up

---
+++ app2.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+A routing layer for the onboarding bot tutorial built using
+[Slack's Events API](https://api.slack.com/events-api) in Python
+"""
+import json
+import bot
+from flask import Flask, request, make_response, render_template
+
+pyBot = bot.Bot()
+slack = pyBot.client
+
+app = Flask(__name__)
+
+def _event_handler(event_type, slack_event):
+    """
+    A helper function that routes events from Slack to our Bot
+    by event type and subtype.
+
+    Parameters
+    ----------
+    event_type : str
+        type of event recieved from Slack
+    slack_event : dict
+        JSON response from a Slack reaction event
+
+    Returns
+    ----------
+    obj
+        Response object with 200 - ok or 500 - No Event Handler error
+
+    """
+    if event_type == "message":
+        try:
+            link = slack_event["event"]["text"]
+            channel = slack_event["event"]["channel"]
+            #check if this is a link
+            if 'http' in link:
+                pyBot.no_grateful_dead(link = link, channel = channel)
+                return make_response("No Grateful Dead!", 200,)
+        except KeyError:
+            return make_response("No Grateful Dead!", 200,)
+    else:
+        # ============= Event Type Not Found! ============= #
+        # If the event_type does not have a handler
+        message = "You have not added an event handler for the %s" % event_type
+        # Return a helpful error message
+        return make_response(message, 500, {"X-Slack-No-Retry": 1})
+
+    return ''
+
+
+@app.route("/listening", methods=["GET", "POST"])
+def hears():
+    """
+    This route listens for incoming events from Slack and uses the event
+    handler helper function to route events to our Bot.
+    """
+    slack_event = json.loads(request.data)
+
+    # ============= Slack URL Verification ============ #
+    # In order to verify the url of our endpoint, Slack will send a challenge
+    # token in a request and check for this token in the response our endpoint
+    # sends back.
+    #       For more info: https://api.slack.com/events/url_verification
+    if "challenge" in slack_event:
+        return make_response(slack_event["challenge"], 200, {"content_type":
+                                                             "application/json"
+                                                             })
+
+    # ============ Slack Token Verification =========== #
+    # We can verify the request is coming from Slack by checking that the
+    # signing secret in the request matches our app's settings
+    if pyBot.verification != slack_event.get("token"):
+        message = "Invalid Slack signing secret"
+        # By adding "X-Slack-No-Retry" : 1 to our response headers, we turn off
+        # Slack's automatic retries during development.
+        return make_response(message, 403, {"X-Slack-No-Retry": 1})
+
+    # ====== Process Incoming Events from Slack ======= #
+    # If the incoming request is an Event we've subcribed to
+    if "event" in slack_event:
+        event_type = slack_event["event"]["type"]
+        # Then handle the event by event_type and have your bot respond
+        return _event_handler(event_type, slack_event)
+    # If our bot hears things that are not events we've subscribed to,
+    # send a quirky but helpful error response
+    return make_response("[NO EVENT IN SLACK REQUEST] These are not the droids\
+                         you're looking for.", 404, {"X-Slack-No-Retry": 1})
+@app.route("/thanks", methods=["GET", "POST"])
+def thanks():
+    """
+    This route is called by Slack after the user installs our app. It will
+    exchange the temporary authorization code Slack sends for an OAuth token
+    which we'll save on the bot object to use later.
+    To let the user know what's happened it will also render a thank you page.
+    """
+    # Let's grab that temporary authorization code Slack's sent us from
+    # the request's parameters.
+    code_arg = request.args.get('code')
+    # The bot's auth method to handles exchanging the code for an OAuth token
+    pyBot.auth(code_arg)
+    return render_template("thanks.html")
+
+
+@app.route("/install", methods=["GET"])
+def pre_install():
+    """This route renders the installation page with 'Add to Slack' button."""
+    # Since we've set the client ID and scope on our Bot object, we can change
+    # them more easily while we're developing our app.
+    client_id = pyBot.oauth["client_id"]
+    scope = pyBot.oauth["scope"]
+    # Our template is using the Jinja templating language to dynamically pass
+    # our client id and scope
+    return render_template("install.html", client_id=client_id, scope=scope)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=4390)
